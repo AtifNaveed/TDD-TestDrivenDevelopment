f5a003774a38072f15799728d7de49ffc131e857
“Atif <atif.gcucs@gmail.com>
2019-09-24 15:34:39 +0400

    First TDD

---
+++ TDD_Swift/TDD_Swift/CalculateSum.swift
@@ -0,0 +1,15 @@
+//
+//  CalculateSum.swift
+//  TDD_Swift
+//
+//  Created by Atif on 24/09/2019.
+//  Copyright © 2019 Atif. All rights reserved.
+//
+
+import UIKit
+
+class CalculateSum {
+    func sum(numbers: String) -> Int {
+        return 0
+    }
+}
--- TDD_Swift/TDD_SwiftTests/TDD_SwiftTests.swift
+++ TDD_Swift/TDD_SwiftTests/TDD_SwiftTests.swift
@@ -7,8 +7,10 @@
 //
 
 import XCTest
+@testable import TDD_Swift
 
 class TDD_SwiftTests: XCTestCase {
+    let calculate = CalculateSum()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,16 +20,10 @@ class TDD_SwiftTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddEmpty() {
+        let sum = calculate.sum(numbers: "")
+        XCTAssertEqual(sum, 0, "Sum was incoreect")
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
